7f3badda7e1bc0c2f026f75f72432f204f8dcfac
Harshithabh1004 <harshithabh370@gmail.com>
2025-08-16 03:02:27 +0530

    WIP ticke_view widget and home_screen; Added assets,widget,styles folder,Created bottomnavbar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,78 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app_project/base/res/styles/app_styles.dart';
+import 'package:ticket_app_project/base/res/styles/media.dart';
+import 'package:ticket_app_project/base/res/widgets/app_double_text.dart';
+import 'package:ticket_app_project/base/res/widgets/ticket_view.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //we want scrollable effect
+      backgroundColor: AppStyles.bgColor,
+      body: ListView(
+        children: [
+          SizedBox(height: 40),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good Morning", style: AppStyles.headLineStyle3),
+                        SizedBox(height: 5),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(AppMedia.logo),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFFF4F6FD),
+                  ),
+                  child: Row(
+                    //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+                      Text("Search"),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 40),
+                AppDoubleText(
+                  bigText: 'Upcoming Flights',
+                  smallText: 'View all',
+                ),
+                // const SizedBox(height: 40,),
+                TicketView()
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
